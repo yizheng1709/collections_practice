419282e9c671e6541218c5b3b505efe474d175a6
Eva <yi.zheng1709@gmail.com>
2021-01-13 22:39:39 -0500

    commit tree 1ed46cc7eef5d2fbd59d9e8be44ab4f9a5ed42a5

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,54 @@
+def sort_array_asc(array)
+    sorted = array.sort 
+    return sorted  
+end 
 
+def sort_array_desc(array)
+    reversed = (array.sort).reverse
+    return reversed 
+end  
+
+def sort_array_char_count(array) 
+    new_array = array.sort_by {|word| word.length}
+    new_array 
+end 
+
+def swap_elements(array)
+    second = array[1]
+    third = array[2]
+    array[1] = third  
+    array[2] = second
+    array 
+end  
+
+def reverse_array(array)
+    reversed = array.reverse 
+    reversed 
+end 
+
+def kesha_maker(array)
+    array.each do |word|
+        word[2] = "$"
+    end 
+    array 
+end
+
+def find_a(array)
+    array.select do |word|
+        word.start_with?("a")
+    end 
+end 
+
+def sum_array(array)
+    array.inject {|sum, n| sum + n}
+end 
+
+def add_s(array) 
+    array.each_with_index.collect do |word, index| 
+        if index != 1
+            word = word + "s"
+        else 
+            word 
+        end 
+    end 
+end 
